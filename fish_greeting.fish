7dffe89fb2c9a845e0858ba290864c62af7357c7
sosuke-k <snoopies.drum@gmail.com>
2015-12-03 06:56:59 +0900

    fish greets google hourly hot trends

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -2,5 +2,7 @@ function fish_greeting -d "what's up, fish?"
   set_color $fish_color_autosuggestion[1]
   uname -npsr
   uptime
+  echo "Google hourly hot trends"
+  curl -s -S http://www.google.co.jp/trends/hottrends/atom/hourly | grep "<li>" | sed -e 's/<[^>]*>//g'
   set_color normal
 end
